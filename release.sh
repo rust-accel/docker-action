2d4b8025e72c6bd6e896f1992fc79f6dc595a0e1
Toshiki Teramura <toshiki.teramura@gmail.com>
2020-01-05 23:47:37 +0900

    Remove submodules

--- release.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -eu
-
-for tag in $(git submodule | cut -f 3 -d " "); do
-  cd $tag
-    git tag ${RELEASE}/$tag
-    git push --tags
-  cd -
-done
--- upgrade.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -xue
-
-for tag in $(git submodule | cut -f 3 -d " "); do
-  cd $tag
-    git fetch
-    git reset origin/$tag --hard
-  cd -
-done
